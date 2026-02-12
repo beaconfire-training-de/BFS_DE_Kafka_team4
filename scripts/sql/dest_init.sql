8fac2ed7c3c5f0662bd0df76571d601e8889f0ad
Brandon Scott Buana <54853904+BrandonScottt@users.noreply.github.com>
2026-02-11 23:24:58 -0500

    Add files via upload

---
+++ scripts/sql/dest_init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS employees(
+    emp_id INT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    dob DATE,
+    city VARCHAR(100),
+    salary INT
+);
