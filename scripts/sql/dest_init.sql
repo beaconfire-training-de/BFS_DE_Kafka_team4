e28b437532fe0be845ed5c2f172b3f955e096b90
Brandon Scott Buana <54853904+BrandonScottt@users.noreply.github.com>
2026-02-13 10:19:50 -0500

    Delete scripts/sql directory

--- scripts/sql/dest_init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS employees(
-    emp_id INT PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    dob DATE,
-    city VARCHAR(100),
-    salary INT
-);
